088ad002f1147dd5537fb9963a08f6fd1dbcd6a2
Evilboom <1049142293@qq.com>
2021-05-29 15:29:44 +0800

    0529 修改为多标签loss

--- model/recmodel.py
+++ model/recmodel.py
@@ -4,6 +4,7 @@
 # @Version：V 0.0
 # @File : recmodel.py
 # @desc :
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from transformers import BertModel, BertConfig
@@ -39,25 +40,26 @@ class RecModel(nn.Module):
 
     def forward(self, inputs, attention_mask, labels=None):
         outputs, pooled = self.bert(inputs, attention_mask=attention_mask)
-        out, h_n = self.bi_gru(outputs)
-        sequence_output = out[:, -1]
+        # ******************** gru
+        # out, h_n = self.bi_gru(outputs)
+        # sequence_output = out[:, -1]
         # ********************* 加CNN
         # outputs = outputs.unsqueeze(1)
         # outputs = torch.cat([self.conv_and_pool(outputs, conv) for conv in self.multi_conv], 1)
         # sequence_output = outputs
         # *********************
         # 无CNN的时候
-        # sequence_output = pooled
+        sequence_output = pooled
         # *********************
         sequence_output = self.dropout(sequence_output)
         # gru
-        rel_out = self.gru_lin1(sequence_output)
+        # rel_out = self.gru_lin1(sequence_output)
         # ********************* 无CNN
-        # rel_out = self.lin1(sequence_output)
+        rel_out = self.lin1(sequence_output)
         # ********************* 有CNN
         # rel_out = self.f_cnn(sequence_output)
         # *********************
-        # rel_out = rel_out.sigmoid()
+        rel_out = rel_out.sigmoid()
         outputs = (rel_out,)
 
         if labels is not None:
@@ -65,7 +67,24 @@ class RecModel(nn.Module):
             loss_mask = labels.gt(0)
             # loss = F.binary_cross_entropy(rel_out, labels.float(), reduction='none')
             # bce 加 sigmoid 综合
-            loss = self.loss_f(rel_out, labels.float())
-            loss = (loss * loss_mask.float()).sum() / loss_mask.float().sum()
+            # loss = self.loss_f(rel_out, labels.float())
+            loss = self.mul_cat_cross(labels, rel_out)
+            loss = loss.sum()/len(loss)
+            # loss = (loss * loss_mask.float()).sum() / loss_mask.float().sum()
             outputs = (loss,) + outputs
         return outputs
+
+    def mul_cat_cross(self, y_true, y_pred):
+        """多标签分类的交叉熵
+        说明：y_true和y_pred的shape一致，y_true的元素非0即1，
+             1表示对应的类为目标类，0表示对应的类为非目标类。
+        """
+        y_pred = (1 - 2 * y_true) * y_pred
+        y_pred_neg = y_pred - y_true * 1e12
+        y_pred_pos = y_pred - (1 - y_true) * 1e12
+        zeros = torch.zeros_like(y_pred[..., :1])
+        y_pred_neg = torch.cat([y_pred_neg, zeros], dim=-1)
+        y_pred_pos = torch.cat([y_pred_pos, zeros], dim=-1)
+        neg_loss = torch.logsumexp(y_pred_neg, dim=-1)
+        pos_loss = torch.logsumexp(y_pred_pos, dim=-1)
+        return neg_loss + pos_loss
